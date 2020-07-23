5a895f4a1ebc339821eca684860d3b9cef5bba79
Matthew Callery <callerymatthew@gmail.com>
2020-07-23 09:31:34 +0100

    Refactor DogBreedsAdapter.kt ViewHolder class

--- app/src/main/java/com/example/chipdogshowcase/DogBreedsAdapter.kt
+++ app/src/main/java/com/example/chipdogshowcase/DogBreedsAdapter.kt
@@ -1,31 +1,44 @@
 package com.example.chipdogshowcase
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class DogBreedsAdapter : RecyclerView.Adapter<DogBreedViewHolder>() {
+class DogBreedsAdapter : RecyclerView.Adapter<DogBreedsAdapter.ViewHolder>() {
     var data = listOf<DogBreed>()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
+    class ViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val dogBreedName: TextView = itemView.findViewById(R.id.dog_breeds_item_view_name)
+
+        fun bind(item: DogBreed) {
+            dogBreedName.text = item.breedName
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val view = layoutInflater.inflate(R.layout.dog_breeds_item_view, parent, false)
+                return ViewHolder(view)
+            }
+        }
+    }
+
     override fun getItemCount(): Int {
         return data.size
     }
 
-    override fun onBindViewHolder(holder: DogBreedViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = data[position]
-        holder.textView.text = item.breedName
+        holder.bind(item)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogBreedViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.dog_breeds_item_view, parent, false) as TextView
-        return DogBreedViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder.from(parent)
     }
 }
-
-class DogBreedViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
