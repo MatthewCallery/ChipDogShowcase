f956e9f32f1ebb593ffaf53697c9f0d71e8ceac7
Matthew Callery <callerymatthew@gmail.com>
2020-07-24 16:52:44 +0100

    Add support for an Up button

--- app/src/main/java/com/example/chipdogshowcase/MainActivity.kt
+++ app/src/main/java/com/example/chipdogshowcase/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.chipdogshowcase
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.chipdogshowcase.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val navController = findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
