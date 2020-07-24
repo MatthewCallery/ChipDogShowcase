a21ed03cf2985e0aa043a7c38ef5c85b44265884
Matthew Callery <callerymatthew@gmail.com>
2020-07-24 10:30:27 +0100

    Create bindImage binding adapter in BindingUtils.kt

--- app/src/main/java/com/example/chipdogshowcase/BindingUtils.kt
+++ app/src/main/java/com/example/chipdogshowcase/BindingUtils.kt
@@ -1,9 +1,22 @@
 package com.example.chipdogshowcase
 
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 
 @BindingAdapter("dogBreedName")
 fun TextView.setDogBreedName(item: DogBreed) {
     text = item.breedName
+}
+
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUri = it.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUri)
+            .into(imgView)
+    }
 }
