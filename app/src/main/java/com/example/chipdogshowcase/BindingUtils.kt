d35e01a72c89c979d0008aea5ef9353c52a76b18
Matthew Callery <callerymatthew@gmail.com>
2020-07-24 12:08:11 +0100

    Refactor DogBreedsFragment breeds observer into binding adapter

--- app/src/main/java/com/example/chipdogshowcase/BindingUtils.kt
+++ app/src/main/java/com/example/chipdogshowcase/BindingUtils.kt
@@ -4,11 +4,19 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 
 @BindingAdapter("dogBreedName")
 fun TextView.setDogBreedName(item: DogBreed) {
     text = item.breedName
+}
+
+// Init DogBreedsAdapter with list of DogBreed objects
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView, data: List<DogBreed>?) {
+    val adapter = recyclerView.adapter as DogBreedsAdapter
+    adapter.submitList(data)
 }
 
 // Take URL from an XML attribute associated with an ImageView, and use Glide to load the image
--- app/src/main/java/com/example/chipdogshowcase/DogBreedsFragment.kt
+++ app/src/main/java/com/example/chipdogshowcase/DogBreedsFragment.kt
@@ -6,7 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.chipdogshowcase.databinding.FragmentDogBreedsBinding
 
@@ -14,7 +13,6 @@ class DogBreedsFragment : Fragment() {
     private lateinit var binding: FragmentDogBreedsBinding
     private lateinit var viewModel: DogBreedsViewModel
     private lateinit var viewModelFactory: DogBreedsViewModelFactory
-    private lateinit var adapter: DogBreedsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,13 +28,7 @@ class DogBreedsFragment : Fragment() {
 
         // RecyclerView and Adapter
         binding.lifecycleOwner = viewLifecycleOwner
-        adapter = DogBreedsAdapter()
-        binding.dogBreedsFragmentList.adapter = adapter
-        viewModel.breeds.observe(viewLifecycleOwner, Observer {
-            it.let {
-                adapter.submitList(it)
-            }
-        })
+        binding.dogBreedsFragmentList.adapter = DogBreedsAdapter()
 
         return binding.root
     }
