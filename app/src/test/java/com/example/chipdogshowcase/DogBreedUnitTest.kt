030ac9c16ce899a8bc6d56a3ecf2616af95161c7
Matthew Callery <callerymatthew@gmail.com>
2020-07-27 18:05:03 +0100

    Add testAddImageUrls to DogBreedUnitTest.kt

--- app/src/test/java/com/example/chipdogshowcase/DogBreedUnitTest.kt
+++ app/src/test/java/com/example/chipdogshowcase/DogBreedUnitTest.kt
@@ -1,6 +1,7 @@
 package com.example.chipdogshowcase
 
 import com.example.chipdogshowcase.models.DogBreed
+import com.example.chipdogshowcase.models.addImageUrlList
 import org.junit.Test
 import org.junit.Assert.*
 
@@ -15,5 +16,22 @@ class DogBreedUnitTest {
 
         // then
         assertEquals(dogBreed.breedName, "Jack Russell")
+    }
+
+    @Test
+    fun testAddImageUrls() {
+        // given
+        val imageUrls = arrayListOf<String>(
+            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg",
+            "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg"
+        )
+        val dogBreed = DogBreed("Spaniel")
+
+        // when
+        dogBreed.addImageUrlList(imageUrls)
+
+        // then
+        assertEquals(dogBreed.imageUrls[0], "https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg")
+        assertEquals(dogBreed.imageUrls[1], "https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg")
     }
 }
