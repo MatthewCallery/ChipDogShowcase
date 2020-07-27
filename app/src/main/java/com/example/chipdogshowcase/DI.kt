cdff1593f3bc937fb8a10698f6b6298e8e935049
Matthew Callery <callerymatthew@gmail.com>
2020-07-27 17:13:00 +0100

    Comment DI dependency injection object

--- app/src/main/java/com/example/chipdogshowcase/DI.kt
+++ app/src/main/java/com/example/chipdogshowcase/DI.kt
@@ -3,6 +3,7 @@ package com.example.chipdogshowcase
 import com.example.chipdogshowcase.network.DogApiService
 import com.example.chipdogshowcase.network.retrofit
 
+// Dependency Injection object containing singletons
 object DI {
     private val savedPreferencesService = SavedPreferencesService(MyApplication.appContext)
     private val retrofitService: DogApiService by lazy {
